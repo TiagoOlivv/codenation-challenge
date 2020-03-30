019c8a98a1743be3259a31d94d75d55fed98e6ec
tiagoolivv <tiago.-oliveira@hotmail.com>
2020-03-30 19:50:42 -0300

    feat: Approved

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 require('dotenv').config()
-
 const axios = require('axios');
 const fs = require('fs');
 const sha1 = require('sha1');
 const FormData = require('form-data');
 
 function cipherCesar(str, num) {
-    
-    if (num > 26 || num < 0) return null;
-
-    if (num === 0) return str;
-
     let output = '';
-
     for (let i = 0; i < str.length; i++) {
         let charcode = str.charCodeAt(i);
-
         if (charcode > 96 && charcode < 123) {
             let newcharcode = charcode - num;
             if (newcharcode < 97) {
@@ -23,43 +15,24 @@ function cipherCesar(str, num) {
             }
             output += String.fromCharCode(newcharcode);
         } else {
-
             output += String.fromCharCode(charcode);
         }
     }
-
     return output;
 }
 
 axios.get(process.env.DATA_TOKEN)
   .then(request => {
     const { data } = request;
-
-    console.log('-- Gerando arquivo answer.json');
-    fs.writeFileSync("answer.json", JSON.stringify(data));
-    console.table(data);
-
-    console.log('\n-- Decifrando o texto cifrado');
+    const response = new FormData();
     data.decifrado = cipherCesar(data.cifrado, data.numero_casas);
-    fs.writeFileSync("answer.json", JSON.stringify(data));
-    console.table(data);
-
-    console.log('\n-- Gerando resumo criptografico do texto decifrado usando algoritmo sha1');
     data.resumo_criptografico = sha1(data.decifrado);
     fs.writeFileSync("answer.json", JSON.stringify(data));    
-    console.table(data);
-
-    console.log('\n-- Submetendo arquivo atualizado');
-    const response = new FormData();
     response.append('answer', fs.createReadStream('answer.json'));
-    
     return axios.post(process.env.SOLUTION_TOKEN,
     	response,
-    	{ headers: response.getHeaders() }
-    ).then(() => {
-    	console.log('Arquivo submetido com sucesso!');
-    }).catch(err => {
-    	console.log('Houve um erro ao submeter o arquivo.');
-    })
+    	{ 
+            headers: response.getHeaders() 
+        }
+    )
   })
-
